c4596c11e0cc2b8cdc3cc3056e0437068581ebef
unknown <andanivicente@gmail.com>
2020-02-10 12:59:45 +0100

    Map finish

--- module/client/module/contact/view/inc/contact_top_page.php
+++ module/client/module/contact/view/inc/contact_top_page.php
@@ -1,3 +1,5 @@
 <!--PROPIO CONTACT-->
 <link rel="stylesheet" href="module/client/module/contact/view/css/contact_style.css">
 <script type="text/javascript" src="module/client/module/contact/controller/ccontact.js"></script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyClXM2-qxMMHYesj4T7Qer4DeRzSIcaiJ4&callback=initMap" type="text/javascript"></script>
+<script type="text/javascript" src="module/client/module/contact/view/js/map.js"></script>
